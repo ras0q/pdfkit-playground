c51f3751eb36627057b27f679bfb5b66afafd36c
ras0q <ras0q@users.noreply.github.com>
2024-04-24 02:43:46 +0900

    :sparkles: impl PDF Viewer with PDFKit

--- Sources/Feature/ContentView.swift
+++ Sources/Feature/ContentView.swift
@@ -1,19 +1,36 @@
+import PDFKit
 import SwiftUI
 
 public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        PDFKitView(path: "1706.03762")
     }
 }
 
+struct PDFKitView: UIViewRepresentable {
+    private var url: URL
+
+    private var pdfView: PDFView = {
+        let pdfView = PDFView()
+        pdfView.autoScales = true
+        pdfView.displaysPageBreaks = false
+        return pdfView
+    }()
+
+    init(path: String) {
+        self.url = Bundle.module.url(forResource: path, withExtension: "pdf")!
+        pdfView.document = PDFDocument(url: url)
+    }
+
+    func makeUIView(context: Context) -> some UIView {
+        self.pdfView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
 #Preview {
-    ContentView()
+    PDFKitView(path: "1706.03762")
 }
