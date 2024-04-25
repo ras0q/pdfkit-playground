264f9987df971a5513167e2fb04471678f67c4f8
ras0q <ras0q@users.noreply.github.com>
2024-04-26 00:23:08 +0900

    :bug: fix and write to each page successfully

--- Sources/Feature/PDFKitView.swift
+++ Sources/Feature/PDFKitView.swift
@@ -12,8 +12,10 @@ struct PDFKitView: UIViewRepresentable {
         )!
     }
 
-    func makeUIView(context: Context) -> PDFView {
-        let pdfView = PDFView()
+    func makeUIView(context: Context) -> UIView {
+        let rootView = UIView()
+
+        let pdfView = CanvasPDFView()
         pdfView.autoScales = true
         pdfView.displaysPageBreaks = true
         pdfView.delegate = context.coordinator
@@ -23,62 +25,73 @@ struct PDFKitView: UIViewRepresentable {
         document?.delegate = context.coordinator
         pdfView.document = document
 
-        return pdfView
+        rootView.addSubview(pdfView)
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pdfView.topAnchor.constraint(equalTo: rootView.topAnchor),
+            pdfView.bottomAnchor.constraint(equalTo: rootView.bottomAnchor),
+            pdfView.leftAnchor.constraint(equalTo: rootView.leftAnchor),
+            pdfView.rightAnchor.constraint(equalTo: rootView.rightAnchor)
+        ])
+
+
+        // MARK: workaround to display PKToolPicker
+        let emptyCanvasView = PKCanvasView()
+        emptyCanvasView.isHidden = true
+        pdfView.toolPicker.addObserver(emptyCanvasView)
+        pdfView.toolPicker.setVisible(true, forFirstResponder: emptyCanvasView)
+        emptyCanvasView.becomeFirstResponder()
+
+        rootView.addSubview(emptyCanvasView)
+
+        return rootView
     }
 
-    func updateUIView(_ pdfView: PDFView, context: Context) {}
+    func updateUIView(_ view: UIView, context: Context) {}
 
     func makeCoordinator() -> CanvasPDFCoordinator {
         Coordinator()
     }
 }
 
-class CanvasPDFCoordinator: UIView {
-    var pageToViewMapping = [PDFPage: PKCanvasView]()
-}
+class CanvasPDFCoordinator: NSObject {}
 
 extension CanvasPDFCoordinator: PDFPageOverlayViewProvider {
     func pdfView(_ pdfView: PDFView, overlayViewFor page: PDFPage) -> UIView? {
-        pageToViewMapping[page] ?? {
-            // FIXME: 一番最後にvisibleになったページにしか書き込めない
-            // FIXME: 進んで戻ると消えてる
-            let canvasView = PKCanvasView(frame: page.bounds(for: pdfView.displayBox))
-            canvasView.drawingPolicy = .anyInput
+        guard 
+            let pdfView = (pdfView as? CanvasPDFView),
+            let page = (page as? CanvasPDFPage)
+        else {
+            return nil
+        }
+
+        let canvasView = pdfView.pageToViewMapping[page] ?? {
+            let canvasView = PKCanvasView(frame: .zero)
+            canvasView.drawingPolicy = .pencilOnly
             canvasView.backgroundColor = .clear
             canvasView.isOpaque = true
             canvasView.clipsToBounds = false
-            canvasView.becomeFirstResponder()
+            canvasView.tool = PKInkingTool(.pen, color: .systemRed, width: 5)
+
             pdfView.addGestureRecognizer(canvasView.drawingGestureRecognizer)
-
-            // FIXME: 表示されない
-            let picker = PKToolPicker()
-            picker.setVisible(true, forFirstResponder: canvasView)
-            picker.addObserver(canvasView)
-
-            pageToViewMapping[page] = canvasView
+            pdfView.pageToViewMapping[page] = canvasView
+            pdfView.toolPicker.addObserver(canvasView)
 
             return canvasView
         }()
+
+        return canvasView
     }
 
     func pdfView(_ pdfView: PDFView, willEndDisplayingOverlayView overlayView: UIView, for page: PDFPage) {
-        // FIXME: not visibleになる際にPDFに書き込もうとしているがうまくいかない
-        guard let drawing = (page as? CanvasPDFPage)?.drawing else { return }
-        let annotation = CanvasPDFAnnotation(
-            bounds: drawing.bounds,
-            forType: .stamp,
-            withProperties: nil
-        )
-        let codedData = try! NSKeyedArchiver.archivedData(
-            withRootObject: drawing,
-            requiringSecureCoding: true
-        )
-        annotation.setValue(
-            codedData,
-            forAnnotationKey: PDFAnnotationKey(rawValue: "drawingData")
-        ) // TODO: what?
+        guard 
+            let overlayView = (overlayView as? PKCanvasView),
+            let page = (page as? CanvasPDFPage)
+        else {
+            return
+        }
 
-        page.addAnnotation(annotation)
+        page.drawing  = overlayView.drawing
     }
 }
 
@@ -90,21 +103,20 @@ extension CanvasPDFCoordinator: PDFDocumentDelegate {
     }
 }
 
-class CanvasPDFPage: PDFPage {
-    var drawing: PKDrawing? = nil
+class CanvasPDFView: PDFView {
+    var pageToViewMapping = [PDFPage: PKCanvasView]()
+    var toolPicker = PKToolPicker()
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if let hitPage = page(for: point, nearest: true) {
+            pageToViewMapping.forEach { (page, canvasView) in
+                canvasView.drawingGestureRecognizer.isEnabled = page == hitPage
+            }
+        }
+        return super.hitTest(point, with: event)
+    }
 }
 
-class CanvasPDFAnnotation: PDFAnnotation {
-    override func draw(with box: PDFDisplayBox, in context: CGContext) {
-        UIGraphicsPushContext(context) // TODO: what?
-        context.saveGState()
-
-        if let drawing = (page as? CanvasPDFPage)?.drawing {
-            let image = drawing.image(from: drawing.bounds, scale: 1)
-            image.draw(in: drawing.bounds)
-        }
-
-        context.restoreGState()
-        UIGraphicsPopContext()
-    }
+class CanvasPDFPage: PDFPage {
+    var drawing: PKDrawing? = nil
 }
